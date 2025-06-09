582bf29d1c5095d618f48181fd2c482ffad98c7c
Viktor Gamov <gamussa@gmail.com>
2025-06-09 15:53:11 +0200

    added test scripts to test with httpie

--- scripts/test-shelly-api.sh
+++ scripts/test-shelly-api.sh
@@ -13,54 +13,54 @@ echo ""
 case "${1:-help}" in
     "on")
         echo "Turning bulb ON with orange color..."
-        curl "http://$BULB_IP/color/0?turn=on&red=255&green=128&blue=64&white=0&gain=80"
+        http GET "http://$BULB_IP/color/0" turn==on red==255 green==128 blue==64 white==0 gain==80
         ;;
     "off")
         echo "Turning bulb OFF..."
-        curl "http://$BULB_IP/color/0?turn=off"
+        http GET "http://$BULB_IP/color/0" turn==off
         ;;
     "color")
         echo "Setting color to purple..."
-        curl "http://$BULB_IP/color/0?red=120&green=80&blue=200&white=0"
+        http GET "http://$BULB_IP/color/0" red==120 green==80 blue==200 white==0
         ;;
     "brightness")
         echo "Setting brightness to 50%..."
-        curl "http://$BULB_IP/color/0?gain=50"
+        http GET "http://$BULB_IP/color/0" gain==50
         ;;
     "timer")
         echo "Setting auto-off timer to 300 seconds..."
-        curl "http://$BULB_IP/color/0?timer=300"
+        http GET "http://$BULB_IP/color/0" timer==300
         ;;
     "toggle")
         echo "Toggling bulb state..."
-        curl "http://$BULB_IP/color/0?turn=toggle"
+        http GET "http://$BULB_IP/color/0" turn==toggle
         ;;
     "status")
         echo "Getting device status..."
-        curl "http://$BULB_IP/status"
+        http GET "http://$BULB_IP/status"
         ;;
     "info")
         echo "Getting device info..."
-        curl "http://$BULB_IP/shelly"
+        http GET "http://$BULB_IP/shelly"
         ;;
     "test-all")
         echo "Running all test commands..."
         echo ""
         echo "1. Device info:"
-        curl "http://$BULB_IP/shelly"
+        http GET "http://$BULB_IP/shelly"
         echo -e "\n\n2. Device status:"
-        curl "http://$BULB_IP/status"
+        http GET "http://$BULB_IP/status"
         echo -e "\n\n3. Turn on with red color:"
-        curl "http://$BULB_IP/color/0?turn=on&red=255&green=0&blue=0&white=0&gain=100"
+        http GET "http://$BULB_IP/color/0" turn==on red==255 green==0 blue==0 white==0 gain==100
         sleep 2
         echo -e "\n\n4. Change to green:"
-        curl "http://$BULB_IP/color/0?red=0&green=255&blue=0&white=0"
+        http GET "http://$BULB_IP/color/0" red==0 green==255 blue==0 white==0
         sleep 2
         echo -e "\n\n5. Change to blue:"
-        curl "http://$BULB_IP/color/0?red=0&green=0&blue=255&white=0"
+        http GET "http://$BULB_IP/color/0" red==0 green==0 blue==255 white==0
         sleep 2
         echo -e "\n\n6. Turn off:"
-        curl "http://$BULB_IP/color/0?turn=off"
+        http GET "http://$BULB_IP/color/0" turn==off
         ;;
     "help"|*)
         echo "Available commands:"
@@ -74,15 +74,15 @@ case "${1:-help}" in
         echo "  info       - Get device info"
         echo "  test-all   - Run full test sequence"
         echo ""
-        echo "Manual curl examples:"
-        echo "  curl \"http://$BULB_IP/color/0?turn=on&red=255&green=128&blue=64&white=0&gain=80\""
-        echo "  curl \"http://$BULB_IP/color/0?turn=off\""
-        echo "  curl \"http://$BULB_IP/color/0?red=120&green=80&blue=200&white=0\""
-        echo "  curl \"http://$BULB_IP/color/0?gain=50\""
-        echo "  curl \"http://$BULB_IP/color/0?timer=300\""
-        echo "  curl \"http://$BULB_IP/color/0?turn=toggle\""
-        echo "  curl \"http://$BULB_IP/status\""
-        echo "  curl \"http://$BULB_IP/shelly\""
+        echo "Manual HTTPie examples:"
+        echo "  http GET \"http://$BULB_IP/color/0\" turn==on red==255 green==128 blue==64 white==0 gain==80"
+        echo "  http GET \"http://$BULB_IP/color/0\" turn==off"
+        echo "  http GET \"http://$BULB_IP/color/0\" red==120 green==80 blue==200 white==0"
+        echo "  http GET \"http://$BULB_IP/color/0\" gain==50"
+        echo "  http GET \"http://$BULB_IP/color/0\" timer==300"
+        echo "  http GET \"http://$BULB_IP/color/0\" turn==toggle"
+        echo "  http GET \"http://$BULB_IP/status\""
+        echo "  http GET \"http://$BULB_IP/shelly\""
         echo ""
         echo "Parameters:"
         echo "  RGB values: 0-255"
